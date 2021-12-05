cf9f187e492930924bd9cbd9232d4f76de2ea92e
Kacper Rolewski <kacper.r17@gmail.com>
2021-12-05 19:30:26 +0100

    Removed the extra window and added drawing the figure to the main window

--- src/test/java/edu/kis/powp/jobs2d/TestJobs2dPatterns.java
+++ src/test/java/edu/kis/powp/jobs2d/TestJobs2dPatterns.java
@@ -40,7 +40,7 @@ public class TestJobs2dPatterns {
 		DriverFeature.getDriverManager().setCurrentDriver(loggerDriver);
 
 		Job2dDriver testDriver = new DriversAdapter();
-		DriverFeature.addDriver("Buggy Simulator", testDriver);
+		DriverFeature.addDriver("Draw Simulator", testDriver);
 
 		DriverFeature.updateDriverInfo();
 	}
@@ -83,7 +83,6 @@ public class TestJobs2dPatterns {
 			public void run() {
 				Application app = new Application("2d jobs Visio");
 				DrawerFeature.setupDrawerPlugin(app);
-				setupDefaultDrawerVisibilityManagement(app);
 
 				DriverFeature.setupDriverPlugin(app);
 				setupDrivers(app);

bc2d490ed109563dfda9f5467e8da23bd032d8ca
Paweł Jędrzejczyk <jedrzejczyk.pawel@outlook.com>
2021-12-05 13:37:07 +0100

    add test figure_joe_2

--- src/test/java/edu/kis/powp/jobs2d/events/SelectTestFigureOptionListener.java
+++ src/test/java/edu/kis/powp/jobs2d/events/SelectTestFigureOptionListener.java
@@ -1,10 +1,10 @@
 package edu.kis.powp.jobs2d.events;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
 import edu.kis.powp.jobs2d.drivers.DriverManager;
 import edu.kis.powp.jobs2d.magicpresets.FiguresJoe;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class SelectTestFigureOptionListener implements ActionListener {
 
@@ -16,6 +16,10 @@ public class SelectTestFigureOptionListener implements ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		FiguresJoe.figureScript1(driverManager.getCurrentDriver());
+		if (e.getActionCommand().equals("Figure Joe 1")){
+			FiguresJoe.figureScript1(driverManager.getCurrentDriver());
+		}else if(e.getActionCommand().equals("Figure Joe 2")){
+			FiguresJoe.figureScript2(driverManager.getCurrentDriver());
+		}
 	}
 }

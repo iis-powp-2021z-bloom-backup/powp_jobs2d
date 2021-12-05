e428ec160aebf529026bac9cafae3013f5632773
Mateusz J <mateusz5314@gmail.com>
2021-12-05 18:15:05 +0100

    Enable usage of figures joe 2

--- src/main/java/edu/kis/powp/jobs2d/drivers/adapter/DrawAdapter.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/DrawAdapter.java
@@ -4,16 +4,15 @@ import edu.kis.legacy.drawer.panel.DrawPanelController;
 import edu.kis.legacy.drawer.shape.ILine;
 import edu.kis.legacy.drawer.shape.LineFactory;
 import edu.kis.powp.jobs2d.Job2dDriver;
-import edu.kis.powp.jobs2d.features.DrawerFeature;
 
 /**
  * driver adapter to drawer with several bugs.
  */
-public class MyAdapter extends DrawPanelController implements Job2dDriver {
+public class DrawAdapter extends DrawPanelController implements Job2dDriver {
 	private int startX = 0, startY = 0;
 	private final DrawPanelController drawPanelController;
 
-	public MyAdapter(DrawPanelController controller) {
+	public DrawAdapter(DrawPanelController controller) {
 		super();
 		drawPanelController = controller;
 	}
@@ -36,6 +35,6 @@ public class MyAdapter extends DrawPanelController implements Job2dDriver {
 
 	@Override
 	public String toString() {
-		return "@Q!$!@$!#@$(*#@&Q(%^*#@";
+		return "Drawer";
 	}
 }

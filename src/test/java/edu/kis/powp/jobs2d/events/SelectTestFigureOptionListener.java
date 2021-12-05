9f32b0332585b0d51cf205813e6781741b01dce6
Karo0510 <karma0510@gmail.com>
2021-12-05 16:34:19 +0100

    dodanie Figure Joe 2

--- src/test/java/edu/kis/powp/jobs2d/events/SelectTestFigureOptionListener.java
+++ src/test/java/edu/kis/powp/jobs2d/events/SelectTestFigureOptionListener.java
@@ -4,6 +4,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import edu.kis.powp.jobs2d.drivers.DriverManager;
+import edu.kis.powp.jobs2d.features.DrawerFeature;
 import edu.kis.powp.jobs2d.magicpresets.FiguresJoe;
 
 public class SelectTestFigureOptionListener implements ActionListener {
@@ -16,6 +17,17 @@ public class SelectTestFigureOptionListener implements ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		FiguresJoe.figureScript1(driverManager.getCurrentDriver());
+
+		if (e.getActionCommand().toString().compareTo("Figure Joe 1") == 0)
+		{
+			DrawerFeature.getDrawerController().clearPanel();
+			FiguresJoe.figureScript1(driverManager.getCurrentDriver());
+		}
+		else if (e.getActionCommand().toString().compareTo("Figure Joe 2") == 0)
+		{
+			DrawerFeature.getDrawerController().clearPanel();
+			FiguresJoe.figureScript2(driverManager.getCurrentDriver());
+		}
+
 	}
 }

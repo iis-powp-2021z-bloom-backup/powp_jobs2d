218aab51d1e28fa3d710b07491525be83b914c12
wioleta1501 <wioleta1501@gmail.com>
2021-12-12 13:36:51 +0100

    added commands package with Driver, OperateTo and SetPosition

--- src/main/java/edu/kis/powp/jobs2d/drivers/adapter/DrawerAdapterImpl.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/DrawerAdapterImpl.java
@@ -9,11 +9,11 @@ import edu.kis.powp.jobs2d.features.DrawerFeature;
 /**
  * driver adapter to drawer with several bugs.
  */
-public class AdapterImpl extends DrawPanelController implements Job2dDriver {
+public class DrawerAdapterImpl extends DrawPanelController implements Job2dDriver {
 	private final DrawerFeature drawerFeature;
 	private int startX = 0, startY = 0;
 
-	public AdapterImpl() {
+	public DrawerAdapterImpl() {
 		super();
 		this.drawerFeature = new DrawerFeature();
 	}

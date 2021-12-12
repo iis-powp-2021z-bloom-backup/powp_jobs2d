16a06a97adc4d80c891d4358b581a8fafc331d39
Mateusz Bebenek <mateusz.bebenek2@gmail.com>
2021-12-12 13:24:16 +0100

    Realizacja dwóch punktów sprawozdania

---
+++ src/main/java/edu/kis/powp/command/ComplexCommand.java
@@ -0,0 +1,11 @@
+package edu.kis.powp.command;
+
+import edu.kis.powp.jobs2d.Job2dDriver;
+
+public class ComplexCommand implements DriverCommand{
+
+    @Override
+    public void execute(Job2dDriver job2dDriver) {
+
+    }
+}

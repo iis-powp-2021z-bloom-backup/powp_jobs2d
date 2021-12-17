5b1a288134615d33a8c9713632f853adad5a5e97
J.Przydalski <przydal@gmail.com>
2021-12-17 22:10:05 +0100

    implement figure builders and factories

---
+++ src/main/java/edu/kis/powp/jobs2d/builders/Figure2Builder.java
@@ -0,0 +1,33 @@
+package edu.kis.powp.jobs2d.builders;
+
+import edu.kis.powp.jobs2d.Job2dDriver;
+import edu.kis.powp.jobs2d.drivers.command.ComplexCommand;
+import edu.kis.powp.jobs2d.drivers.command.DriverCommand;
+import edu.kis.powp.jobs2d.drivers.command.OperateToCommand;
+import edu.kis.powp.jobs2d.drivers.command.SetPositionCommand;
+import edu.kis.powp.jobs2d.magicpresets.FiguresJoe;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Figures2Builder implements Job2dDriver, Builder {
+    private final List<DriverCommand> commandList = new ArrayList<>();
+
+    @Override
+    public void setPosition(int x, int y) {
+        this.commandList.add(new SetPositionCommand(x, y));
+    }
+
+    @Override
+    public void operateTo(int x, int y) {
+        this.commandList.add(new OperateToCommand(x, y));
+    }
+
+    @Override
+    public Object build() {
+        if (this.commandList.size() == 0) {
+            FiguresJoe.figureScript2(this);
+        }
+        return new ComplexCommand(this.commandList);
+    }
+}
---
+++ src/main/java/edu/kis/powp/jobs2d/drivers/factory/FigureScriptFactory.java
@@ -0,0 +1,2 @@
+package edu.kis.powp.jobs2d.drivers.factory;public class FigureScriptFactory {
+}

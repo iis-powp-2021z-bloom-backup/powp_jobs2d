2e7df962e3b4991b76406aca38cf287355cc9d53
Karo0510 <karma0510@gmail.com>
2021-12-18 18:39:41 +0100

    command - zad 3.1.5 - poprawa nazwy

--- src/main/java/edu/kis/powp/jobs2d/drivers/command/ComplexCommand.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/command/ComplexCommand.java
@@ -21,12 +21,7 @@ public class ComplexCommand implements DriverCommand{
 
     @Override
     public void execute() {
-        /*
-        for (int i = 0; i<this.commands.size(); i++)
-        {
-            this.commands.get(i).execute();
-        }
-        */
+
         commands.stream().forEach(t -> t.execute());
     }
 
